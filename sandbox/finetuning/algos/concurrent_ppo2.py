8f94ce9f34eda53e263d5ca7b8797ac4de64d9c6
Andrew Chong Li <acli@edu.uwaterloo.ca>
2020-06-30 13:01:21 -0400

    WIP

--- sandbox/finetuning/algos/concurrent_ppo2.py
+++ sandbox/finetuning/algos/concurrent_ppo2.py
@@ -174,20 +174,15 @@ class Concurrent_PPO(BatchPolopt):
         self.hi_lo_magnitude = sum([x.norm(2) ** 2 for x in self.second_order_grad_hi_lo]) ** 0.5
         self.lo_hi_magnitude = sum([x.norm(2) ** 2 for x in self.second_order_grad_lo_hi]) ** 0.5 
 
-        # self.hi_lo_clipped = lasagne.updates.total_norm_constraint(self.second_order_grad_hi_lo, 0.3)
-        # self.lo_hi_clipped = lasagne.updates.total_norm_constraint(self.second_order_grad_lo_hi, 0.1)
+        self.hi_lo_clipped = lasagne.updates.total_norm_constraint(self.second_order_grad_hi_lo, 0.01)
+        self.lo_hi_clipped = lasagne.updates.total_norm_constraint(self.second_order_grad_lo_hi, 0.01)
 
-        # self.hi_lo_clipped_magnitude = sum([x.norm(2) ** 2 for x in self.hi_lo_clipped]) ** 0.5
-        # self.lo_hi_clipped_magnitude = sum([x.norm(2) ** 2 for x in self.lo_hi_clipped]) ** 0.5
-
-        self.updates_1 = lasagne.updates.adam(self.second_order_grad_hi_lo, self.policy.manager.get_params(trainable=True), learning_rate=self.step_size_2)
-        self.updates_2 = lasagne.updates.adam(self.second_order_grad_lo_hi, self.policy.low_policy.get_params(trainable=True), learning_rate=self.step_size_2)
+        self.updates_1 = lasagne.updates.adam(self.hi_lo_clipped, self.policy.manager.get_params(trainable=True), learning_rate=self.step_size_2)
+        self.updates_2 = lasagne.updates.adam(self.lo_hi_clipped, self.policy.low_policy.get_params(trainable=True), learning_rate=self.step_size_2)
 
         # Updates params, and outputs the magnitude of the gradient. 
         self.train_fn_1 = theano.function([obs_var_raw, action_var, latent_var, obs_var_sparse, latent_var_sparse, advantage_var], updates = self.updates_1, outputs=self.hi_lo_magnitude)
         self.train_fn_2 = theano.function([obs_var_raw, action_var, latent_var, advantage_var, obs_var_sparse, latent_var_sparse, advantage_var_sparse],updates = self.updates_2, outputs=self.lo_hi_magnitude)
-
-        print("INIT HERE", self.step_size_2)
 
         return dict()
 
