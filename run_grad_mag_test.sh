993515884b2fb6b1636ada4cdb38c32cf9b76b50
Andrew Chong Li <acli@edu.uwaterloo.ca>
2020-08-02 15:19:05 -0400

    WIP

--- run_grad_mag_test.sh
+++ run_grad_mag_test.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
+#SBATCH --array=0-29
+#SBATCH --mem=20G
+#SBATCH --cpus-per-task=16
+#SBATCH --partition cpu
 
-for SLURM_ARRAY_TASK_ID in {0..29}
-do
-	idx=$SLURM_ARRAY_TASK_ID
-	bs=$((idx / 15))
-	idx=$((idx % 15))
-	itr=$((idx / 5))
-	idx=$((idx % 5))
-	seed=$((idx))
 
-	if [ $bs -eq 0 ]
-	then
-		bs=160000
-	else
-		bs=480000
-	fi
+idx=$SLURM_ARRAY_TASK_ID
+bs=$((idx / 15))
+idx=$((idx % 15))
+itr=$((idx / 5))
+idx=$((idx % 5))
+seed=$((idx))
 
-	if [ $itr -eq 0 ]
-	then
-		itr=0
-	elif [ $itr -eq 1 ]
-	then
-		itr=200
-	fi
-	
-	echo "$bs, $itr, $seed"
-	%python3 sandbox/finetuning/runs/pg_test.py --env_name snakegather --algo hippo -trainsnn -p 10 -tpi 10 -e 1 -n_itr 0 -eps 0.1 -bs $bs -d 0.999 -mb -sbl -msb -lr2 0 -version 1 --pkl_path data/local/$seed/itr_$itr.pkl
+if [ $bs -eq 0 ]
+then
+	bs=160000
+else
+	bs=480000
+fi
 
-done
+if [ $itr -eq 0 ]
+then
+	itr=0
+elif [ $itr -eq 1 ]
+then
+	itr=200
+else
+	itr=1000
+fi
+
+echo "$bs, $itr, $seed"
+
+python3 sandbox/finetuning/runs/pg_test.py --env_name snakegather --algo hippo -trainsnn -p 10 -tpi 10 -e 1 -n_itr 0 -eps 0.1 -bs $bs -d 0.999 -mb -sbl -msb -lr2 0 -version 1 --pkl_path data/local/$seed/itr_$itr.pkl
